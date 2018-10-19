29f380014d43c9327d438e0c4ac76946f46f8a5d
ColetteSacha <sacha.colette@student.uclouvain.be>
2018-10-19 21:04:21 +0200

    recever sans  buffer

--- read_write_loop.c
+++ read_write_loop.c
@@ -28,13 +28,9 @@ modification : lit sur le socket et renvoie des ack et nack
             pkt_t* renvoi = pkt_new();
 
             
-pkt_status_code read_write_loop(int sfd, pkt* renvoi) {
-    int ret=-1;
-    int totalLengthr=0;
-    int totalLengthwSfd=0;
-    int totalLengthw=0;
+pkt_status_code read_write_loop(int sfd, pkt* renvoi, int fd) {
     char reader[528];
-    memset(reader,0,528);
+
     while(1)
     {
         struct pollfd fds;
@@ -44,16 +40,21 @@ pkt_status_code read_write_loop(int sfd, pkt* renvoi) {
         if (ret<0) {
             fprintf(stderr,"select error\n");
             fprintf(stderr,"ERROR: %s\n", strerror(errno));
-            return;
+            exit(EXIT_FAILURE);
         }
 
         if (fds.revents & POLLIN){
+            memset(reader,0,528);
             int length=read(sfd, reader, 528); // 528 est la taille totale du payload(512) + header(16)
             if(length<12)//la taille est plus petite que le header
             {
                 fprintf(stderr,"ERROR: %s\n", strerror(errno));
                 fprintf(stderr,"Erreur read socket\n");
-                return;
+                return E_UNCONSISTENT;
+            }
+
+            if(length == 0){//fin du programme
+                return PKT_OK;
             }
 
             pkt_t* recu = pkt_new();
@@ -66,7 +67,16 @@ pkt_status_code read_write_loop(int sfd, pkt* renvoi) {
             if(stat != PKT_OK){ // le paquet doit être ignoré
                 return stat;
             }
-            return PKT_OK;
+
+            int w = write(fd, recu->payload, recu->length);
+            if(w != -1){ 
+                fprintf(stderr, "erreur dans l'écriture du recever \n");
+                exit(EXIT_FAILURE);
+            }
+            if(w != recu->length){
+                return E_LENGTH;
+            }
+            
         }
     }
 }
--- recever.c
+++ recever.c
@@ -1,16 +1,29 @@
-
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/poll.h>
 
 /*
-Renvoie 1 si le paquet renvoi doit être renvoyé. Renvoie 0 si le paquet doit être ignoré.
-data est le char* recu du réseau et non décodé. Renvoi est le paquet de réponse à data. recu
-est un paquet vide qui va être rempli pour être lu par la suite.
-window est la taille de la fenêtre actuel du receveur. timestamp n'est pas encore défini
-si la fonction renvoie 1, le paquet recu est valide et doit être lu.
+la fonction place dans renvoi le paquet ACK ou NACK qui devra être renvoyé. 
+Si la fonction renvoit autre chose que PKT_OK le paquet doit être ignoré
+recu est le paquet recu du socket et auquel il faut répondre, window est 
+la taille de la fenetre actuel du receveur. timestamp n'est pas encore défini
 */
 
 
 
-pkt_status_code reponse (pkt_t* recu, pkt_t *renvoi, pkt_t *recu, uint8_t window, uint32_t timestamp){
+pkt_status_code reponse (pkt_t* recu, pkt_t *renvoi, uint8_t window, uint32_t timestamp){
 	int res = 1;
 
 	if(pkt_get_type(recu) != PTYPE_DATA){
@@ -45,3 +58,92 @@ pkt_status_code reponse (pkt_t* recu, pkt_t *renvoi, pkt_t *recu, uint8_t window
 		return PKT_OK;
 	}
 }
+
+
+
+//Avec l'aide de Louis Colin et Jonathan Thibaut
+/* 
+la fonction lit sur le socket et calcul le paquet qu'il faut répondre
+ACK ou NACK. Si la fonction renvoie autre chose que PKT_OK, le paquet recu
+doit être ignoré. 
+sdf est le file descripteur du socket lu. pkt est le paquet de réponse 
+*/
+            
+
+            
+pkt_status_code read_write_loop(int sfd, int fd) {
+    char reader[528];
+
+    while(1)
+    {
+        struct pollfd fds;
+        pkt_t* renvoi;
+        renvoi = pkt_new(); // ne pas oublier de free
+        fds.fd = sfd;
+        fds.events = POLLIN;
+        ret = poll(fds, 1, -1 );
+        if (ret<0) {
+            fprintf(stderr,"select error\n");
+            fprintf(stderr,"ERROR: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        if (fds.revents & POLLIN){
+            memset(reader,0,528);
+            int length=read(sfd, reader, 528); // 528 est la taille totale du payload(512) + header(16)
+            if(length<12)//la taille est plus petite que le header
+            {
+                fprintf(stderr,"ERROR: %s\n", strerror(errno));
+                fprintf(stderr,"Erreur read socket\n");
+                return E_UNCONSISTENT;
+            }
+
+            if(length == 0){//fin du programme
+                return PKT_OK;
+            }
+
+            pkt_t* recu = pkt_new();
+            pkt_status_code stat = pkt_decode(reader, length, recu);
+            if(stat != PKT_OK){
+                return stat;
+            }
+            int rep;
+            stat = reponse(recu, renvoi, 31, 0)// taille max du window, timestamp à voir ??
+            if(stat != PKT_OK){ // le paquet doit être ignoré
+                return stat;
+            }
+
+            int w = write(fd, recu->payload, recu->length);
+            if(w != -1){ 
+                fprintf(stderr, "erreur dans l'écriture du recever \n");
+                exit(EXIT_FAILURE);
+            }
+            if(w != recu->length){
+                return E_LENGTH;
+            }
+            
+        }
+    }
+}
+
+/*
+lit le socket, renvoie un ACK ou NACK, écrit sur l'entrée standard et dans le fichier
+*/
+void recever(int sfd, char* nomFichier){
+	int fd = open(nomFichier, O_WRONLY);
+	if(fd == -1){
+		fprintf(stderr, "erreur dans l'ouverture du fichier de sortie\n");
+		exit(EXIT_FAILURE); // il faut voir les consignes
+	}
+
+	pkt_status_code status;
+	status = read_write_loop(sfd, fd);
+	
+	int f = close(fd);
+	if(fd == -1){
+		fprintf(stderr, "erreur dans la fermeture du fichier de sortie\n");
+	}
+	return;
+
+}
+
