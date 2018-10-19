708075a1b48367caa9a00f0e9de5f13f38847573
loucol1 <l.colin@student.uclouvain.be>
2018-10-19 16:26:21 +0200

    ajout de node.c et node.h

---
+++ node.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+#include "packet_interface.h"
+#include "node.h"
+
+
+
+
+
+
+
+
+node_t* new_node(){
+  node_t *ret=(node_t*)calloc(1,sizeof(node_t));
+  if(ret==NULL){
+      return NULL;
+  }
+  return ret;
+}
+
+
+void node_del(node_t* n){
+  if(n!=NULL){
+    if(n->data!=NULL){
+      pkt_del(n->data);
+    }
+    free(n);
+    n=NULL;
+  }
+}
+
+void node_set_data(node_t *node,pkt_t *new){
+  node->data=new;
+}
+
+void node_set_next(node_t *node,node_t* newNext){
+  node->next=newNext;
+}
+
+pkt_t* node_get_data(node_t *node){
+  return node->data;
+}
+
+node_t* node_get_next(node_t *node){
+  return node->next;
+}
+
+
+
+
+
+
+node_t* create_empty_list(int number)
+{
+  node_t* ret=new_node();
+  if(number==1){
+    return ret;
+  }
+  node_t*current=ret;
+  for(int i=1;i<number;i++){
+    node_t* new=new_node();
+    current->next=new;
+    current=current->next;
+  }
+  current->next=ret;
+  return ret;
+}
---
+++ node.h
@@ -0,0 +1,28 @@
+#ifndef __NODE_H_
+#define __NODE_H_
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+//ATEENTION, INCLUDE LE TRUC DES PKT
+
+
+
+
+
+typedef struct node {
+pkt_t *data;
+struct node *next;
+}node_t;
+
+
+
+node_t* new_node();
+void node_del();
+void node_set_data(node_t *node,pkt_t *new);
+void node_set_next();
+pkt_t* node_get_data(node_t *node);
+node_t* node_get_next(node_t *node);
+node_t* create_empty_list(int number);
+
+#endif
